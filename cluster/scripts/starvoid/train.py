904891ff1ab5fbec53ab94e1bc4f2a0195bdc018
tibuch <tibuch@mpi-cbg.de>
2019-03-13 19:11:50 +0100

    Add starvoid train.py

--- cluster/scripts/starvoid/train.py
+++ cluster/scripts/starvoid/train.py
@@ -9,8 +9,8 @@ import pickle
 
 def add_boundary_label(lbl, dtype=np.uint16):
     """ lbl is an integer label image (not binarized) """
-    b = find_boundaries(lbl,mode='outer')
-    res = (lbl>0).astype(dtype)
+    b = find_boundaries(lbl, mode='outer')
+    res = (lbl > 0).astype(dtype)
     res[b] = 2
     return res
 
@@ -25,30 +25,30 @@ def onehot_encoding(lbl, n_classes=3, dtype=np.uint32):
 
 
 def cutHalf(data, repeat):
-    data_=data
+    data_ = data
     print(data_.shape)
     for i in range(repeat):
-        newsize=int(data_.shape[1]/2)
-        a=data_[:,:newsize,:]
-        b=data_[:,newsize:,:]
-  #      print(a.shape,b.shape)
-        data_=np.concatenate((a,b), axis=0)
+        newsize = int(data_.shape[1] / 2)
+        a = data_[:, :newsize, :]
+        b = data_[:, newsize:, :]
+        #      print(a.shape,b.shape)
+        data_ = np.concatenate((a, b), axis=0)
 
-        newsize=int(data_.shape[2]/2)
-        a=data_[:,:,:newsize]
-        b=data_[:,:,newsize:]
-        data_=np.concatenate((a,b), axis=0)
+        newsize = int(data_.shape[2] / 2)
+        a = data_[:, :, :newsize]
+        b = data_[:, :, newsize:]
+        data_ = np.concatenate((a, b), axis=0)
         print(data_.shape)
     return data_
 
 
 def normalize(img, mean, std):
     zero_mean = img - mean
-    return zero_mean/std
+    return zero_mean / std
 
 
 def denormalize(x, mean, std):
-    return x*std + mean
+    return x * std + mean
 
 
 def convert_to_oneHot(data):
@@ -61,11 +61,9 @@ def convert_to_oneHot(data):
 with open('experiment.json', 'r') as f:
     exp_params = json.load(f)
 
-
-#Config from json file
-with open(exp_params['model_name']+'/config.json', 'r') as f:
+# Config from json file
+with open(exp_params['model_name'] + '/config.json', 'r') as f:
     conf = json.load(f)
-
 
 use_denoising = conf['use_denoising']
 
@@ -79,7 +77,48 @@ mean, std = np.mean(X_train), np.std(X_train)
 X_train = normalize(X_train, mean, std)
 X_val = normalize(X_val, mean, std)
 
-# Select fraction
+# convert to oneHot
+Y_train_oneHot = convert_to_oneHot(Y_train)
+Y_val_oneHot = convert_to_oneHot(Y_val)
 
-train_frac = int(np.round((exp_params['train_frac']/100)*X_train.shape[0]))
-X_train[train_frac:]
+Y_train = np.concatenate(
+    (X_train[..., np.newaxis], np.zeros(X_train.shape, dtype=np.float32)[..., np.newaxis], Y_train_oneHot), axis=3)
+
+# Select fraction
+train_frac = int(np.round((exp_params['train_frac'] / 100) * X_train.shape[0]))
+if use_denoising:
+    Y_train[train_frac:, ..., 1:] *= 0
+else:
+    X_train = X_train[:train_frac]
+    Y_train = Y_train[:train_frac]
+
+if 'augment' in exp_params.keys():
+    if exp_params['augment']:
+        print('augmenting training data')
+        X_ = X_train.copy()
+        X_train = np.concatenate((X_train, np.rot90(X_, 2, (1, 2))))
+        X_train = np.concatenate((X_train, np.flip(X_train)))
+        print('Training data size after augmentation', X_train.shape)
+
+# prepare validation data
+X_validation = X_val[..., np.newaxis]
+Y_validation = Y_val[..., np.newaxis]
+
+num_pix = conf['n2v_num_pix']
+pixelsInPatch = conf['n2v_patch_shape'][0] * conf['n2v_patch_shape'][1]
+
+# 1. Option
+Y_validation = np.concatenate((X_validation.copy(), np.zeros(X_validation.shape, dtype=np.float32)), axis=3)
+manipulate_val_data(X_validation, Y_validation, num_pix=int(num_pix * X_validation.shape[1] * X_validation.shape[2] / float(pixelsInPatch)),
+                    shape=(X_validation.shape[1], X_validation.shape[2]))
+
+Y_validation = np.concatenate((Y_validation, Y_val_oneHot), axis=3)
+
+model = CARE(None, name= exp_params['model_name'], basedir= exp_params['base_dir'])
+print(conf)
+
+hist = model.train(X_train,Y_train,validation_data=(X_validation,Y_validation))
+
+with open(join(exp_params['base_dir'], exp_params['model_name'], 'history_' + exp_params['model_name'] + '.dat'),
+          'wb') as file_pi:
+    pickle.dump(hist.history, file_pi)
